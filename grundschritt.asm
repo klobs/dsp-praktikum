a2e7b8b80f70de55b9cbd426db732e94b26e836c
klobs <jw@klobs.de>
2010-01-12 23:35:12 +0100

    Bug fixes:
    	- Tabs durch Leerzeichen ersetzen, weil Tabs eine Semantik haben.
    	- Parallele operationen durch mehrere einzelne ersetzen, weil parallele Operationen nicht auf ein Register zur gleichen Zeit zugreifen koennen
    	- Syntax fuer Adressangaben korregiert

--- grundschritt.asm
+++ grundschritt.asm
@@ -1,42 +1,49 @@
 ;Start Grundschritt
 ;1. Schritt: Skalierung
-GRUNDSCHRITT	MOVE	X:(R1),X1 Y:(R1),Y1;
-				MPY 	0.5,X1, A; Skalierung, speichern in A
-				MPY 	0.5,Y1, B; Skalierung, speichern in B
-				MOVE	A,X:(R1) B,Y:(R1);
+GRUNDSCHRITT MOVE X:(R1),X1 
+    MOVE Y:(R1),Y1;
+    MPY  0.5,X1, A; Skalierung, speichern in A
+    MPY  0.5,Y1, B; Skalierung, speichern in B
+    MOVE A,X:(R1);
+    MOVE B,Y:(R1);
 
-				MOVE	X:(R4),X1 Y:(R4),Y1; (Ggf unnoetig)
-				MPY		0.5,X1, A; Skalierung, speichern in A
-				MPY		0.5,Y1, B; Skalierung, speichern in B
-				MOVE	A,X:(R4) B:Y(R4);
+    MOVE X:(R4),X1;(Ggf unnoetig)
+    MOVE Y:(R4),Y1; 
+    MPY  0.5,X1, A; Skalierung, speichern in A
+    MPY  0.5,Y1, B; Skalierung, speichern in B
+    MOVE A,X:(R4);
+    MOVE B,Y:(R4);
 
 ;2. Schritt: Twiddlefaktor applizieren
-				MOVE	A, X1; Sollte sich seit letztem Move nicht veraendert haben
-				MOVE 	B, Y1; sonst nochmal aus dem X-Speicher kopieren
+    MOVE A, X1; Sollte sich seit letztem Move nicht veraendert haben
+    MOVE B, Y1; sonst nochmal aus dem X-Speicher kopieren
 
-				; (a+jb)*(c+jd) = (ac - bd)+j(bc+ad)
+    ; (a+jb)*(c+jd) = (ac - bd)+j(bc+ad)
 
-				MPY		X0,X1, A; Realer Teil
-				MAC		-Y0,Y1,A; (Hinzuadieren)
+    MPY  X0,X1,A; Realer Teil
+    MAC  -Y0,Y1,A; (Hinzuadieren)
 
-				MPY		Y0,X1, B; Imaginaerer Teil
-				MAC		X,Y1,  B; (Hinzuadieren)
+    MPY  Y0,X1,B; Imaginaerer Teil
+    MAC  X,Y1,B; (Hinzuadieren)
 
-				MOVE	A,X:(R4) B,Y:(R4); Wegsichern
+    MOVE A,X:(R4); Wegsichern
+    MOVE B,Y:(R4); 
 
 ;3. Schritt: Berechnung
-				ADD		X:(R1), A;
-				ADD		Y:(R1), B;
-				MOVE	A,X:(R2) B,Y:(R2); Wegsichern
+    ADD  X:(R1), A;
+    ADD  Y:(R1), B;
+    MOVE A,X:(R2); Wegsichern
+    MOVE B,Y:(R2); 
 
-				MOVE	X:(R1), A; Alte Ergebnisse fuer neue Berechnung zurueck holen
-				MOVE	Y:(R1), B;
-	
-				SUB		X:(R4),A;
-				SUB		Y:(R4),B;
+    MOVE X:(R1), A; Alte Ergebnisse fuer neue Berechnung zurueck holen
+    MOVE Y:(R1), B;
+ 
+    SUB  X:(R4),A;
+    SUB  Y:(R4),B;
 
-				MOVE	A,X:(R5) B,Y:(R5);
+    MOVE A,X:(R5);
+    MOVE B,Y:(R5);
 
 ;ENDE Grundschritt
-				
-				RTS
+    
+    RTS;
--- rahmen.asm
+++ rahmen.asm
@@ -1,23 +1,23 @@
 ;Rahmenprogramm Start
 ;Initialisierung
 
-ORG	P:$000;
+ORG	P:$100;
 INCLUDE butterfly.asm; Problem: das Programm laeuft 
 					 ; noch ab der falschen stelle los!!
 
 ;Schritt 1: Outputadressen einstellen
 
-MOVEC 	$00C,R2; Frage: wie wird hier der X, bzw Y Speicher angegeben?
-MOVEC 	$00F,R5;
+MOVE 	#$00C,R2; Frage: wie wird hier der X, bzw Y Speicher angegeben?
+MOVE 	#$00F,R5;
 
 ;Schritt 2: Input Adressen vorgeben
-MOVEC 	$000,R1;
-MOVEC 	$006,R4;
+MOVE 	#$000,R1;
+MOVE 	#$006,R4;
 
 ;Schritt 3: Twiddlefaktor justieren
 
-MOVEC 	$1,X0;
-MOVEC 	$0,Y0;
+MOVE 	$1,X0;
+MOVE 	$0,Y0;
 
 ;Schritt 4: Grundschritt aufrufen
 
@@ -25,12 +25,14 @@ JSR 	GRUNDSCHRITT;
 
 ;Schritt 5: Ergebnisse sichern
 
-MOVEC	(R2), $000;
-MOVEC	(R5), $006;
+MOVE	(R2), X:$000;
+MOVE	(R2), Y:$000;
+MOVE	(R5), X:$006;
+MOVE	(R5), Y:$006;
 
 ;Schritt 6: Neue Input Adressen vorgeben
-MOVEC	$003,R1;
-MOVEC	$00F,R4;
+MOVE	#$003,R1;
+MOVE	#$00F,R4;
 
 ;Schritt 7: Grundschritt aufrufen
 
@@ -38,13 +40,15 @@ JSR	GRUNDSCHRITT;
 
 ;Schritt 8: Ergebnisse sichern
 
-MOVEC 	(R2), $003;
-MOVEC 	(R5), $00F;
+MOVE 	(R2), X:$003;
+MOVE 	(R2), Y:$003;
+MOVE 	(R5), X:$00F;
+MOVE 	(R5), Y:$00F;
 
 ;Schritt 9: Inputadressen vorgeben
 
-MOVEC	$000, R1;
-MOVEC	$003, R4;
+MOVE	#$000, R1;
+MOVE	#$003, R4;
 
 ;Schritt 10: Grundschritt aufrufen
 
@@ -52,18 +56,20 @@ JSR	GRUNDSCHRITT;
 
 ;Schritt 11: Ergebnisse sichern
 
-MOVEC	(R2), $000;
-MOVEC	(R5), $003;
+MOVE	(R2), X:$000;
+MOVE	(R2), Y:$000;
+MOVE	(R5), X:$003;
+MOVE	(R5), Y:$003;
 
 ;Schritt 12: Twiddlefaktor anpassen
 
-MOVEC	$0, X0;
-MOVEC	-1, Y0;
+MOVE	$0, X0;
+MOVE	-1, Y0;
 
 ;Schritt 13: Inputadressen Vorgeben
 
-MOVEC	$006,R1;
-MOVEC	$009,R4;
+MOVE	#$006,R1;
+MOVE	#$009,R4;
 
 ;Schritt 14: Grundschritt aufrufen
 
@@ -71,7 +77,9 @@ JSR 	GRUNDSCHRITT;
 
 ;Schritt 15: Ergebnisse sichern
 
-MOVEC	(R2), $006;
-MOVEC	(R2), $009;
+MOVE	(R2), X:$006;
+MOVE	(R2), Y:$006;
+MOVE	(R2), X:$009;
+MOVE	(R2), Y:$009;
 
 ;ENDE
